888dd32c9b74bffbf3fd768fad750d8c62e01a37
prov <provinite@gmail.com>
2017-02-21 23:07:07 -0600

    Adds CORS

--- src/main/java/com/clovercoin/pillowing/PillowingBankApplication.java
+++ src/main/java/com/clovercoin/pillowing/PillowingBankApplication.java
@@ -2,11 +2,25 @@ package com.clovercoin.pillowing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class PillowingBankApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PillowingBankApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**");
+			}
+		};
 	}
 }
